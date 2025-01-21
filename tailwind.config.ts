1411814e410e492c281ad14d50cf72ab9ed349ea
leconstantin <lcon69184@gmail.com>
2025-01-21 18:28:15 +0200

    cleaned up it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        bgColor: "#5DC9A8"
       },
     },
   },
